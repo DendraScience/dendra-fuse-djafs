c4a82af2f9e733b3fffbdfa398690b8d134cd719
Tai Groot <tai@taigrr.com>
2023-08-01 15:48:09 -0700

    add main back to vcs

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
+	_ "bazil.org/fuse/fs/fstestutil"
+
+	archivefs "github.com/dendrascience/dendra-archive-fuse/fs"
+)
+
+const (
+	SemVer  string = "1.0.0"
+	Package string = "dendra_archive_fuse"
+)
+
+var (
+	Authors   string
+	BuildNo   string
+	BuildTime string
+	GitCommit string
+	Tag       string
+
+	hostname string
+	version  = flag.Bool("version", false, "Get detailed version string and exit")
+)
+
+func init() {
+	flag.Parse()
+
+	Authors = strings.ReplaceAll(Authors, "SpAcE", " ")
+	Tag = strings.ReplaceAll(Tag, ";", "; ")
+
+	if GitCommit == "" || BuildTime == "" {
+		log.Fatalf("Binary built improperly. Version variables not set!")
+	}
+	fmt.Printf("%s Version information:\n|| Authors: %s\n|| Commit: %s\n|| Tag: %s\n|| Build No: %s\n|| Build Date: %s\n", Package, Authors, GitCommit, Tag, BuildNo, BuildTime)
+
+	if *version {
+		os.Exit(0)
+	} else {
+		fmt.Printf("Initialization success...\n")
+	}
+}
+
+func main() {
+	flag.Usage = help
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		help()
+		os.Exit(2)
+	}
+	mountpoint := flag.Arg(0)
+
+	c, err := fuse.Mount(
+		mountpoint,
+		fuse.FSName("djafs"),
+		fuse.Subtype("djafs"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	err = fs.Serve(c, archivefs.NewFS())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func help() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "  %s MOUNTPOINT\n", os.Args[0])
+	flag.PrintDefaults()
+}
