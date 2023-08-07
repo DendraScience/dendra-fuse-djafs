5bde739259597bb58c19d8bfcd2fadd8de55db98
Tai Groot <tai@taigrr.com>
2023-08-06 17:51:29 -0700

    add inode incrementer

---
+++ util/inode.go
@@ -0,0 +1,29 @@
+package util
+
+import "sync"
+
+var (
+	highestInode uint64 = 0
+	inodeLock           = sync.Mutex{}
+)
+
+func GetNewInode() uint64 {
+	inodeLock.Lock()
+	defer inodeLock.Unlock()
+	highestInode++
+	return highestInode
+}
+
+func SeeInode(inode uint64) {
+	go func(i uint64) {
+		inodeLock.Lock()
+		defer inodeLock.Unlock()
+		if i > highestInode {
+			highestInode = i
+		}
+	}(inode)
+}
+
+func FileNameFromInode(inode uint64) string {
+	return ""
+}
